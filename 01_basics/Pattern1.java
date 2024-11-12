a76b85b78a5c4f45e60631ee0f25d25ac78c150a
AbhaySharma0017 <abhay.svce@gmail.com>
2024-11-12 12:31:11 +0530

    Pattern

---
+++ 01_basics/Pattern1.java
@@ -0,0 +1,19 @@
+class Pattern1
+{
+    public static void main(String args[]){
+
+       for(int i=1; i<=9; i++){
+        for(int j=1; j<=9; j++){
+            if((i<=5 && j==1) || (i<=9 && j==9)||(i==5 || j ==5)||(i==9 && j<=4)|| (i==1 && j<=9))
+            {
+                System.out.print("*");
+            }
+            else{
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+       }
+    }
+
+}
