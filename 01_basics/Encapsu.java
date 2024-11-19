a84c44f53e22306f6837fdba2a4e20416ed4db7d
AbhaySharma0017 <abhay.svce@gmail.com>
2024-11-19 15:24:38 +0530

    program

---
+++ 01_basics/Encapsu.java
@@ -0,0 +1,25 @@
+class Encapsu{
+    private int a;
+    private int b;
+
+   public void setData(int x, int y){
+        a = x;
+        b = y;
+    }
+    void add(){
+            System.out.print(a+b);
+        }
+
+    public static void main(String args[]){
+        Encapsu e = new Encapsu();
+        e.setData(10,20);
+        e.add();
+        int g = 10;
+        int h = 20;
+         static void multi(){
+            System.out.print(g*h);
+        }
+        Encapsu.void();
+       
+    }
+}
