26902ef48ecd76a36486ae25aa708fca696d36db
AbhaySharma0017 <abhay.svce@gmail.com>
2024-11-27 07:49:24 +0530

    Some updation

---
+++ Abstraction/AbsConstructor.java
@@ -0,0 +1,31 @@
+// Abstract Class Having Constructor, Data Member and Methods
+// An abstract class can have a data member, abstract method, method body (non-abstract method), constructor, and even main() method.
+
+abstract class Bike{
+    Bike(){
+        System.out.println("Bike constructor is created.");
+    }
+    abstract void run();
+    void move(){
+        System.out.println("Bike is started.");
+    }
+}
+
+class jawa extends Bike{
+        void run(){
+            System.out.println("Bike is running mode.");
+        }
+
+        // void move(){
+        //     System.out.println("Bike is stop");
+        // } (We are also override concreate method);
+}
+
+class AbsConstructor{
+    public static void main(String args[])
+    {
+        Bike b = new jawa();
+        b.run();
+        b.move();
+    }
+}
---
+++ Abstraction/AbsInterface.java
@@ -0,0 +1,39 @@
+// Another Real Scenario of Abstract Class
+// The abstract class can also be used to provide some implementation of the interface. In such case, the end user may not be forced to override all the methods of the interface.
+
+interface I{
+    void a1();
+    void b1();
+    void c1();
+    void d1(); 
+}
+
+abstract class M implements I{
+    public void a1(){
+        System.out.println("a1 method");
+    }
+}
+
+class N extends M{
+      public void b1(){
+         System.out.println("b1 method");
+      }
+
+     public void c1(){
+        System.out.println("c1 method");
+      }
+     public void d1(){
+        System.out.println("d1 method");
+      }
+}
+
+class AbsInterface{
+    public static void main(String args[])
+    {
+        I i = new N();
+        i.a1();
+        i.b1();
+        i.c1();
+        i.d1();
+    }
+}
