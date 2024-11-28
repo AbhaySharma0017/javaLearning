235ab2c71a1a0efdfc95e80a328865fd07e5b6f2
AbhaySharma0017 <abhay.svce@gmail.com>
2024-11-28 23:32:28 +0530

    Interface

--- Interface/I1.java
+++ Interface/I1.java
@@ -2,8 +2,20 @@
  interface Animal{
      void eat();
     void sleep();
+   // void information(); // Is method ko override karna necessary hai 
 
     public static final int MAX_AGE = 100;
+
+   // Static method
+     public static void info(){ // Is method ko override karna necessary nhi hai
+        System.out.println("This is an Animal interface");
+    }
+  // Default method
+     public default void run(){
+    this.eat();
+     System.out.println("This is running");
+ }
+
 }
 
 class Dog implements Animal{
@@ -15,6 +27,7 @@ class Dog implements Animal{
    public void sleep(){
         System.out.println("Dog is sleeping.");
     }
+
 }
 
 class Cat implements Animal{
@@ -31,7 +44,8 @@ class I1{
        Dog d = new Dog();
        d.eat();
        d.sleep();
-
+       Animal.info();
+       d.run();
        System.out.println("Age "+new Cat().MAX_AGE);
        System.out.println("Age "+Animal.MAX_AGE);
     }
