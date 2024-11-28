23ee7a990a61d8e9bebca91464f8b34b1b0bfdf2
AbhaySharma0017 <abhay.svce@gmail.com>
2024-11-29 00:20:54 +0530

    Main inside the interface

---
+++ Interface/Main3.java
@@ -0,0 +1,10 @@
+interface Main3{
+    void run();
+    public static void main(String... args){
+        System.out.println("We can write main method inside the interface, because it allow to write static mrthod inside the interface and main method is also static");
+
+        int a = 10;
+        int b = 20;
+        System.out.println(a+b);
+    }
+}
