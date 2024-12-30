24d35633ff56376bedae8f91d5504cad205920c8
AbhaySharma0017 <your-email@example.com>
2024-12-30 11:22:08 +0530

    Package

---
+++ Packege/mypack1/I1.java
@@ -0,0 +1,5 @@
+public interface I1{
+    static void m1(){
+        System.out.println("Inside I1");
+    }
+}
---
+++ Packege/pack/Demo11.java
@@ -0,0 +1,6 @@
+class Demo11{
+    public static void main(String args[])
+    {
+        I1.m1();
+    }
+}
